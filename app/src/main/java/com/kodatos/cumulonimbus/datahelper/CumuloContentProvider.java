dfb38d2b8b882a3ed5fbb6915df5e785fedc250e
Kodatos <abswamy97@gmail.com>
2018-03-27 19:21:16 +0530

    Add copyright to relevant files and update gradle

--- app/src/main/java/com/kodatos/cumulonimbus/datahelper/CumuloContentProvider.java
+++ app/src/main/java/com/kodatos/cumulonimbus/datahelper/CumuloContentProvider.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 N Abhishek (aka Kodatos)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.kodatos.cumulonimbus.datahelper;
 
 
@@ -55,12 +79,12 @@ public class CumuloContentProvider extends ContentProvider {
         Cursor retCursor;
         switch (code){
             case WEATHER_DATA : retCursor = db.query(WeatherDBEntry.TABLE_NAME,projection,selection,selectionArgs,null,null,sortOrder);
-                                break;
+                break;
             case WEATHER_WITH_ID :  String id = uri.getPathSegments().get(1);
-                                    String mselection = "_id=?";
-                                    String[] margs = new String[]{id};
-                                    retCursor = db.query(WeatherDBEntry.TABLE_NAME,projection,mselection,margs,null,null,sortOrder);
-                                    break;
+                String mselection = "_id=?";
+                String[] margs = new String[]{id};
+                retCursor = db.query(WeatherDBEntry.TABLE_NAME, projection, mselection, margs, null, null, sortOrder);
+                break;
             default: throw new UnsupportedOperationException("Unknown Uri "+uri.toString());
         }
         retCursor.setNotificationUri(getContext().getContentResolver(),uri);
@@ -87,8 +111,7 @@ public class CumuloContentProvider extends ContentProvider {
                     db.setTransactionSuccessful();
                     retUri = WeatherDBEntry.CONTENT_URI.buildUpon().appendPath(String.valueOf(id)).build();
                     db.endTransaction();
-                }
-                else {
+                } else {
                     db.endTransaction();
                     throw new SQLException("Insert Failed!");
                 }
@@ -140,8 +163,8 @@ public class CumuloContentProvider extends ContentProvider {
             case WEATHER_DATA : retno = db.update(WeatherDBEntry.TABLE_NAME,values,selection,selectionArgs);
                 if (retno < 0) {
                     Log.w("CONTENT_PROVIDER", "Update Failed!");
-                                }
-                                break;
+                }
+                break;
             default: throw new UnsupportedOperationException("Unknown Uri "+uri.toString());
         }
         return retno;
--- app/src/main/java/com/kodatos/cumulonimbus/datahelper/WeatherDBHelper.java
+++ app/src/main/java/com/kodatos/cumulonimbus/datahelper/WeatherDBHelper.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 N Abhishek (aka Kodatos)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.kodatos.cumulonimbus.datahelper;
 
 
@@ -5,7 +29,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.kodatos.cumulonimbus.datahelper.WeatherDBContract.*;
+import com.kodatos.cumulonimbus.datahelper.WeatherDBContract.WeatherDBEntry;
 
 public class WeatherDBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "weather_db.db";
@@ -22,6 +46,7 @@ public class WeatherDBHelper extends SQLiteOpenHelper {
             WeatherDBEntry.COLUMN_ICON_ID+" TEXT NOT NULL, "+
             WeatherDBEntry.COLUMN_UV_INDEX+" REAL NOT NULL, "+
             WeatherDBEntry.COLUMN_RAIN_3H+" REAL NOT NULL);";
+
     public WeatherDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
